07bd95f976d4298e2362dba3115e425e8c0071e4
mainsplainer <46342367+mainsplainer@users.noreply.github.com>
2023-06-28 21:45:09 +0545

    Refactor and rename 1. EXTRACT.ps1 to 1. DST EXTRACT.ps1

---
+++ 1. DST EXTRACT.ps1
@@ -0,0 +1,48 @@
+$DFL_PATH = Get-Content -Path "paths.txt" -First 1
+
+function GetVideos($path) {
+	$extensions = @('.mp4','.avi','.wmv', '.mkv', '.mov', '.mpg', '.mpeg', '.webm', '.m4v', '.3gp', '.3gpp')
+	return Get-ChildItem -Path $path\workspace | Where-Object {$_.Extension -in $extensions}
+}
+
+function DstExtract() {
+	Set-Location $DFL_PATH
+	
+	Write-Host "STEP (1/3): Video Frame Extraction" -ForegroundColor Yellow 
+	echo "jpg" | & .\"3) extract images from video data_dst FULL FPS.bat"
+	
+	Write-Host "STEP (2/3): Faceset Extraction" -ForegroundColor Yellow 
+	echo `n | & .\"5) data_dst faceset extract.bat"
+	
+	Write-Host "STEP (3/3): Applying Mask" -ForegroundColor Yellow 
+	echo `n | & .\"5.XSeg) data_dst trained mask - apply.bat"
+}
+
+$index = 0
+$dst_videos = GetVideos $DFL_PATH
+
+foreach ($video in $dst_videos) {
+	$index++
+	Write-Host ("VIDEO ({0:d3}/{1:d3}). {2}" -f $index, $dst_videos.count, $video.Name) -ForegroundColor Green
+
+	Set-Location $DFL_PATH
+	$folder_name   = [System.IO.Path]::GetFileNameWithoutExtension($video)
+	$new_file_name = "data_dst{0}" -f [System.IO.Path]::GetExtension($video)
+	$video | Rename-Item -NewName $new_file_name
+	
+	DstExtract $fps
+
+	Set-Location $PSScriptRoot
+	python "helpers/1. DST EXTRACT (Clean Up).py" --dfl_path $DFL_PATH --old_video_name $video
+
+	# Results in access denied error so needed to wait 5-seconds
+	# Set-Location $DFL_PATH\workspace
+	# New-Item -ItemType Directory $folder_name | Out-Null
+	# Move-Item $new_file_name $folder_name
+	# Start-Sleep -Seconds 5
+	# Move-Item "data_dst" $folder_name
+}
+
+RunDLL32 User32.dll,MessageBeep
+Write-Host "Finished! (You may close this window!)" -ForegroundColor Green
+pause
