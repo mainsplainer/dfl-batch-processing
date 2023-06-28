a2b143a2144d7c47296b5bbebc769f9b84418009
mainsplainer <46342367+mainsplainer@users.noreply.github.com>
2023-06-28 21:48:16 +0545

    Create 0. SRC EXTRACT.ps1

---
+++ 0. SRC EXTRACT.ps1
@@ -0,0 +1,38 @@
+$DFL_PATH = Get-Content -Path "paths.txt" -First 1
+
+function GetVideos($path) {
+	$extensions = @('.mp4','.avi','.wmv', '.mkv', '.mov', '.mpg', '.mpeg', '.webm', '.m4v', '.3gp', '.3gpp')
+	return Get-ChildItem -Path $path\workspace | Where-Object {$_.Extension -in $extensions}
+}
+
+function SrcExtract($fps) {
+	Set-Location $DFL_PATH
+	
+	Write-Host "STEP (1/2): Video Frame Extraction" -ForegroundColor Yellow 
+	echo $fps | & .\"2) extract images from video data_src.bat"
+	
+	Write-Host "STEP (2/2): Faceset Extraction" -ForegroundColor Yellow 
+	echo `n | & .\"4) data_src faceset extract.bat"
+}
+
+$fps = Read-Host "Enter FPS";
+$index = 0
+$src_videos = GetVideos $DFL_PATH
+
+foreach ($video in $src_videos) {
+	$index++
+	Write-Host ("VIDEO ({0:d3}/{1:d3}). {2}" -f $index, $src_videos.count, $video.Name) -ForegroundColor Green
+	
+	Set-Location $DFL_PATH
+	$new_file_name = "data_src{0}" -f [System.IO.Path]::GetExtension($video)
+	$video | Rename-Item -NewName $new_file_name
+	
+	SrcExtract $fps
+	
+	Set-Location $PSScriptRoot
+	python "helpers/0. SRC EXTRACT (Clean Up).py" --dfl_path $DFL_PATH --index $index --old_video_name $video
+}
+
+RunDLL32 User32.dll,MessageBeep
+Write-Host "Finished! (You may close this window!)" -ForegroundColor Green
+pause
